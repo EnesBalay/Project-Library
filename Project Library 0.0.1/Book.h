f0e8c418a7dfe31550706ecce5a048a71424663b
EmenCoder <62758732+EmenCoder@users.noreply.github.com>
2020-06-26 16:13:16 +0300

    Add files via upload

---
+++ Project Library 0.0.1/Book.h
@@ -0,0 +1,30 @@
+#ifndef BOOK
+#define	BOOK
+#include<iostream>
+#include<string>
+#include<fstream>
+
+using namespace std;
+class Book {
+private:
+	string bookName, bookWriter, bookPublisher;
+	int bookNo, bookPublishYear, counter;
+public:
+	Book(string/*, string, string, int*/);
+	string getBookName();
+	/*string getBookWriter();
+	string getBookPublisher();
+	int getBookNo();
+	int getBookPublishYear();
+	int getCounter();*/
+	void counterCheck();
+	void counterUp();
+	void counterDown();
+	void counterReset();
+	void addBook();
+	void showBook(); 
+	void checkBook(); 
+	void modifyBook();
+	void deleteBook();
+};
+#endif // !BOOK
